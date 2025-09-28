584ddd563e6b42c33ac89eabed6ff3c7c9278a6a
Sharanyamagesh <sharanyamagesh03@gmail.com>
2025-09-28 16:16:18 +0530

    ArrayProblem-4

---
+++ LeftRotate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class LeftRotate {
+    public static void leftRotate(int[]arr, int k){
+        int n = arr.length;
+        k =k%n;
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr,0,n-1);
+    }
+    public static void reverse(int[]arr, int start, int end){
+        while(start<=end){
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7};
+        int k=3;
+        leftRotate(numbers,k);
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");}
+            }
+        
+    }
+    
--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,5 +1,5 @@
 public class RemoveDuplicates {
-    public static int RemoveDuplicates (int[]arr){
+    public static int removeDuplicates(int[] arr) {
         int n=arr.length;
         int j=0;
         for (int i=1;i<n;i++){
@@ -12,7 +12,7 @@ public class RemoveDuplicates {
     }
     public static void main(String[] args) {
         int[] numbers = {1,1,2,2,3,3,4,4,5,5};
-        int length = RemoveDuplicates(numbers);
+        int length = removeDuplicates(numbers);
         for(int k=0;k<length;k++){
             System.out.print(numbers[k]+ " ");
         }
